1eea04d13880bde3250fe18f54c1391f6394290e
nelio1977 <69060111+nelio1977@users.noreply.github.com>
2022-07-01 16:36:29 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+let total=0
+let cards= []
+const startEl=document.querySelector("#startEl")
+const amountEl=document.querySelector(".amount-bet")
+const cardsEl= document.querySelector("#cardsEl")
+const sumEl= document.querySelector("#sumEl")
+const msgEl= document.querySelector("#msgEl")
+const user= document.querySelector("#user")
+const amount= document.querySelectorAll("#amount")
+const div= document.querySelector("div")
+let isAlive= false;
+let hasBlackJack= false;
+let message= ""
+let theBet= 0
+const users= {
+    name: "User", 
+    cash: 500
+}
+user.textContent= users.name + ":  $ " + users.cash;
+
+const btns= div.getElementsByClassName("btn")
+for(let i=0; i<btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active")
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
+
+function randomCard() {
+    let randomNumber= Math.floor(Math.random() * 13) + 1; 
+    if(randomNumber > 11) {
+        return 10;
+    }else {
+        return randomNumber;  
+    }  
+}
+
+function amountBet(value){
+    theBet = Number(value)
+     amountEl.textContent = "$ " + theBet;
+      startEl.addEventListener("click", ()=> {
+        let firstCard= randomCard();
+        let secondCard= randomCard();
+        cards=[firstCard, secondCard]   
+        total= firstCard + secondCard;
+        renderGame()
+  })
+}
+
+function renderGame() {  
+     cardsEl.textContent = "Cards: ";
+     for(let i=0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+     }
+     sumEl.textContent = "total: " + total 
+
+    if(total === 21) {
+        message= "You got the black jack!"
+        let bet= users.cash += theBet * .98;
+        const newBet= bet.toFixed(2)
+        user.textContent = `User: $ ${newBet}` 
+        theBet= 0;
+        amountEl.textContent = "$ 0";  
+         isAlive= true;
+        hasBlackJack= true;
+
+  } else if(total > 21) {
+        message= "Sorry, you lose. Bet and start game again."
+        let bet= users.cash -= theBet
+        const newBet= bet.toFixed(2)
+        user.textContent = `User: $ ${newBet}`     
+        theBet= 0;
+        amountEl.textContent = "$ 0";
+         isAlive= false;
+        hasBlackJack= false;
+
+  } else if(total < 21) {
+        message= "Want to draw a new card?"
+        isAlive= true;
+        hasBlackJack=false
+  }
+  msgEl.textContent= message;
+}
+
+function newCard(){
+    if(!hasBlackJack && isAlive) {
+        let newCard= randomCard();  
+        cards.push(newCard)
+        total += newCard; 
+        renderGame()
+    }   
+}
