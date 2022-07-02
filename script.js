c6f958d7b19edf61ce830e23f753b05c9172f692
Nelio Memeje <neliomemeje@yahoo.com>
2022-07-02 16:07:47 +0800

    adding styles

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+
 let total=0
 let cards= []
 const startEl=document.querySelector("#startEl")
 const amountEl=document.querySelector(".amount-bet")
 const cardsEl= document.querySelector("#cardsEl")
 const sumEl= document.querySelector("#sumEl")
+const betMsg= document.querySelector("#betMsg")
 const msgEl= document.querySelector("#msgEl")
 const newCard= document.querySelector("#newcardEl")
 const user= document.querySelector("#user")
@@ -11,11 +13,12 @@ const amountBtn= document.querySelectorAll(".btn")
 const div= document.querySelector("div")
 let isAlive= false;
 let hasBlackJack= false;
+let msg= ""
 let message= ""
 let theBet= 0
 const users= {
-    name: "User", 
-    cash: 500
+    name: "User Credits", 
+    cash: 30
 }
 user.textContent= users.name + ":  $ " + users.cash;
 
@@ -37,34 +40,50 @@ function randomCard() {
     }  
 }
 
+function checkBet() {
+    if(users.cash < theBet) {
+         msg= "Sorry, not enough credits to play"; 
+         theBet= 0;        
+       } else if (users.cash >= theBet) {
+         betMsg.textContent= "";
+         amountEl.textContent= " $ " + theBet
+         return theBet;            
+       } 
+       betMsg.textContent= msg;
+       msgEl.textContent= "";
+}
+
 amountBtn.forEach(btn => {
     btn.addEventListener("click", (e) => {
-    console.log(e.target.value)
-    theBet = e.target.value
-     amountEl.textContent = "$ " + theBet;
-    })
+       theBet = e.target.value;
+       checkBet()
+    })     
 }) 
    
 startEl.addEventListener("click", ()=> {
-    let firstCard= randomCard();
-    let secondCard= randomCard();
-    cards=[firstCard, secondCard]   
-    total= firstCard + secondCard;
-    renderGame()
+    if(checkBet()) {
+        let firstCard= randomCard();
+        let secondCard= randomCard();
+        cards=[firstCard, secondCard]    
+        total= firstCard + secondCard;
+        renderGame()
+    } 
   })
 
 function renderGame() {  
+    let cardsMsg= ""
      cardsEl.textContent = "Cards: ";
      for(let i=0; i < cards.length; i++) {
-        cardsEl.textContent += cards[i] + " "
+       cardsMsg += cards[i] + " ";
      }
+     cardsEl.textContent += cardsMsg
      sumEl.textContent = "total: " + total 
 
     if(total === 21) {
         message= "You got the black jack!"
         let bet= users.cash += theBet * .98;
         const newBet= bet.toFixed(2)
-        user.textContent = `User: $ ${newBet}` 
+        user.textContent = `User Credits: $ ${newBet}` 
         theBet= 0;
         amountEl.textContent = "$ 0";  
          isAlive= true;
@@ -74,7 +93,7 @@ function renderGame() {
         message= "Sorry, you lose. Bet and start again."
         let bet= users.cash -= theBet
         const newBet= bet.toFixed(2)
-        user.textContent = `User: $ ${newBet}`     
+        user.textContent = `User Credits: $ ${newBet}`     
         theBet= 0;
         amountEl.textContent = "$ 0";
          isAlive= false;
@@ -93,6 +112,7 @@ newCard.addEventListener("click", () => {
         let newCard= randomCard();  
         cards.push(newCard)
         total += newCard; 
+        checkBet()
         renderGame()
     }   
 })
